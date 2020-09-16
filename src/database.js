35b1dfea1190384a737967fbc644f54512f66e1c
JahzielCabrera <jahziel_oswaldo@hotmail.com>
2020-09-16 15:38:22 -0500

    Create Index DB was deleted

--- src/database.js
+++ src/database.js
@@ -5,8 +5,7 @@ const MONGODB_URI = `${MENU_APP_HOST}/${MENU_APP_DATABASE}`;
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: false
+    useNewUrlParser: true
 })
     .then(db => console.log("Database is connect"))
     .catch(err => {console.log(err)});
