094d51127aa7d4cae835187087ba63dacc9ad5a8
JahzielCabrera <jahziel_oswaldo@hotmail.com>
2020-09-16 16:25:51 -0500

    Stripe user info verification

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -440,6 +440,8 @@ userCtrl.stripeWebHooks = async (req, res) => {
 }
 
 userCtrl.renderSubscriptions = async (req, res) => {
+    console.log(req.user);
+    console.log('User Stripe Info' + req.user.stripe);
     if(req.user.stripe === undefined){
         const basic = await Subscription.findOne({subscriptionName: 'Básico'}).lean();
         const intermediate = await Subscription.findOne({subscriptionName: 'Intermedio'}).lean();
